4e807e0c42076b05f1db11b461cb95baba2c40fd
caioisidio <caioisidio@hotmail.com>
2019-09-17 20:42:04 -0300

    Criando Areas e condominios

---
+++ laravel/database/migrations/2019_09_09_013245_create_condominios_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCondominiosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('condominios', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nome');
+            $table->integer('cnpj');
+            $table->string('endereco');
+            $table->integer('cep');
+            $table->string('cidade');
+            $table->string('bairro');
+            $table->string('uf');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('condominios');
+    }
+}
